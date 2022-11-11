d6e814787880e06ccd2d0c1bf306d78e407f6b0c
Yael <####>
2022-11-11 15:07:06 -0600

    Ando recordando el git, no se preocupe :v

---
+++ librarby.c
@@ -0,0 +1,8 @@
+//
+// Created by yael on 11/11/2022.
+//
+
+#include "librarby.h"
+
+
+//OOOOOOOOOOOO, magia XD
---
+++ librarby.h
@@ -0,0 +1,8 @@
+//
+// Created by yael on 11/11/2022.
+//
+
+#ifndef GAME_LIBRARBY_H
+#define GAME_LIBRARBY_H
+
+#endif //GAME_LIBRARBY_H
