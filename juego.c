228b7098a1fa23976341111e02871f47432ee52c
Yael <####>
2022-11-12 11:05:38 -0600

    Creando archivos, sí

---
+++ juego.c
@@ -0,0 +1,5 @@
+//
+// Created by yael on 12/11/2022.
+//
+
+#include "juego.h"
---
+++ juego.h
@@ -0,0 +1,8 @@
+//
+// Created by yael on 12/11/2022.
+//
+
+#ifndef GAME_JUEGO_H
+#define GAME_JUEGO_H
+
+#endif //GAME_JUEGO_H
