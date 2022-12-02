950853f007eaf9185555f3de7cba87b7a7a8efec
Alberto <alberto.renteriac@iteso.mx>
2022-12-01 18:14:27 -0600

    se hizo la pantalla de titulo y de pausa

---
+++ screen.h
@@ -0,0 +1,16 @@
+#ifndef GAME_SCREEN_H
+#define GAME_SCREEN_H
+
+#include "global.h"
+#include "map.h"
+
+int screen_update(int map[64][64], int bridges[64][64], int *water_temp, Camera2D camara,
+                   Font fuente, Texture2D sprite_w, Texture2D sprite_d, Texture2D sprite_decor);
+
+int screen_title(int map[64][64], int bridges[64][64], int *water_temp, Camera2D camara,
+                 Font fuente, Texture2D sprite_w, Texture2D sprite_d, Texture2D sprite_decor);
+
+int screen_pause(int map[64][64], int bridges[64][64], int *water_temp, Camera2D camara,
+                 Font fuente, Texture2D sprite_w, Texture2D sprite_d, Texture2D sprite_decor);
+
+#endif //GAME_SCREEN_H
